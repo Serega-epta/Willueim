2b74f646f1d6984e84b5d51f3c0586ce7a836513
denzzerr <denisownikita@yandex.ru>
2026-07-31 15:51:54 +0300

    fixed training artrition

--- common/defines/02_RoH_defines.lua
+++ common/defines/02_RoH_defines.lua
@@ -142,6 +142,7 @@ NDefines.NAI.AREA_DEFENSE_SETTING_COASTLINES				= false	--no naval invasion will
 
 NDefines.NMilitary.ATTRITION_EQUIPMENT_LOSS_CHANCE = 0.02 --was 0.1
 NDefines.NMilitary.ATTRITION_EQUIPMENT_PER_TYPE_LOSS_CHANCE = 0.02 --was 0.1
+NDefines.NMilitary.TRAINING_ATTRITION = 0.01
 
 NDefines.NAir.AIR_WING_MAX_STATS_SPEED = 3200 -- was 800
 
